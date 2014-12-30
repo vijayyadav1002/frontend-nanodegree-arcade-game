6b97fdd293c286764de030cca83727e44bbfc14b
TALENTICA-ALL\vijayy <vijayyadav1002@gmail.com>
2014-12-30 13:15:54 +0530

    stopped animation frame

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.dt = 2;
+    this.x = 1;
+    this.y  = y;
 }
 
 // Update the enemy's position, required method for game
@@ -14,6 +17,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    //this.x++;
+    this.x = this.x*101;
+    this.y = this.y*83;
+    //this.render();
 }
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +31,21 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function (y) {
+    Enemy.call(this, y);
+    this.sprite = "images/char-boy.png";
+    this.handleInput = function () {
 
+    };
+}
+Player.prototype = Object.create(Enemy.prototype);
+Player.prototype.constructor = Player;
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var player = new Player(5);
+var allEnemies = [new Enemy(1), new Enemy(2)];
 
 
 // This listens for key presses and sends the keys to your
